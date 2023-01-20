3430319d0034ef3efeaaf14dd04b2dd8347b2bbe
mansi sharma <sharma.mansi9077@gmail.com>
2023-01-20 10:37:26 +0530

    today's update

---
+++ 22. Roller coaster(285).py
@@ -0,0 +1,6 @@
+for _ in range(int(input())):
+    x,h=map(int,input().split())
+    if (x<h):
+        print("NO")
+    else:
+        print("YES")
---
+++ 24.Chef on date(294).py
@@ -0,0 +1,6 @@
+for _ in range(int(input())):
+    x,y=map(int,input().split())
+    if (y>x):
+        print("NO")
+    else:
+        print("YES")
