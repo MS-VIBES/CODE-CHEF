91a30e740e7a4fd2d785d9bf900197bd884efb55
mansi sharma <sharma.mansi9077@gmail.com>
2023-02-08 11:00:10 +0530

    today's update

---
+++ 85. Pass the Exam(424).py
@@ -0,0 +1,9 @@
+for _ in range(int(input())):
+    a,b,c=map(int,input().split())
+    if (a+b+c)>=100:
+        if a>=10 and b>=10 and c>=10:
+            print("PASS")
+        else:
+            print("FAIL")
+    else:
+        print("FAIL")
